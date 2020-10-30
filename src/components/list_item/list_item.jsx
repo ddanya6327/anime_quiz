c88ea9b9f7d641c8aeb421b3c4247f0f3aae1495
ddanya6327 <69433959+ddanya6327@users.noreply.github.com>
2020-10-30 19:11:38 +0900

    add user name in list

--- src/components/list_item/list_item.jsx
+++ src/components/list_item/list_item.jsx
@@ -3,7 +3,7 @@ import styles from "./list_item.module.css";
 import { Link } from "react-router-dom";
 
 const ListItem = ({ item }) => {
-  const { id, title, quizzes } = item;
+  const { id, title, quizzes, user_name } = item;
   const thumbnail = quizzes[0] ? quizzes[0].imageURL : "/images/no_images.jpg";
 
   return (
@@ -16,6 +16,7 @@ const ListItem = ({ item }) => {
         />
       </div>
       <p className={styles.title}>{title}</p>
+      <p className={styles.user_name}>{user_name}</p>
       <div>
         <Link to={`/quiz/${id}`}>
           <button className={styles.button}>퀴즈 시작</button>
