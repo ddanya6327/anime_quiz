c59b49e8f29713ff79487b6d252ebf92ecea35ec
ddanya6327 <69433959+ddanya6327@users.noreply.github.com>
2020-11-16 15:05:31 +0900

    update main

--- src/container/main/main.jsx
+++ src/container/main/main.jsx
@@ -4,32 +4,26 @@ import Header from "../header/header";
 import { Link } from "react-router-dom";
 
 const Main = () => {
-  const test = (event) => {
-    event.currentTarget.innerText = "쳐 누르지마 \n ㅅㅂ";
-  };
-
   return (
     <section className={styles.main}>
       <Header />
       <div className={styles.contents}>
-        <Link to="/list">
-          <button className={styles.mainButton}>퀴즈 리스트</button>
-        </Link>
-        <Link to="/add">
-          <button className={styles.mainButton}>퀴즈 등록</button>
-        </Link>
-        <button className={styles.mainButton} onClick={test}>
-          구색맞추기용 버튼1
-        </button>
-        <button className={styles.mainButton} onClick={test}>
-          구색맞추기용 버튼2
-        </button>
-        <button className={styles.mainButton} onClick={test}>
-          구색맞추기용 버튼3
-        </button>
-        <button className={styles.mainButton} onClick={test}>
-          구색맞추기용 버튼4
-        </button>
+        <div className={styles.mainBox}>
+          <span>みんなで作るAnime Quiz！</span>
+          <Link to="/list">
+            <button>クイズにチャレンジする</button>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.menuBox}>
+        <div className="button">
+          <Link to="/list">
+            <button className={styles.menuButton}>Quiz List</button>
+          </Link>
+          <Link to="/add">
+            <button className={styles.menuButton}>Create Your Quiz</button>
+          </Link>
+        </div>
       </div>
     </section>
   );
