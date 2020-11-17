d3c8caecc4c1f2d24c00f19b73d9317aa252dbb6
ddanya6327 <69433959+ddanya6327@users.noreply.github.com>
2020-11-17 23:59:26 +0900

    fix error conponents file type and change error message

--- src/components/add_errors/errors.jsx
+++ src/components/add_errors/errors.jsx
@@ -4,7 +4,7 @@ import styles from "./errors.module.css";
 const Errors = ({ errors }) => {
   return (
     <div className={styles.errors}>
-      다음 항목을 확인 해주세요.
+      次の項目を確認してください！
       <ul className={styles.error_list}>
         {errors.map((key, index) => (
           <li key={index}>{key}</li>
--- src/components/form_errors/form_errors.jsx
+++ src/components/form_errors/form_errors.jsx
@@ -4,6 +4,7 @@ import styles from "./form_errors.module.css";
 const FormErrors = ({ errors }) => {
   return (
     <div className={styles.errors}>
+      次の項目を確認してください
       <ul className={styles.error_list}>
         {errors.map((key, index) => (
           <li key={index}>{key}</li>
